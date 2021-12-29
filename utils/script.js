746223f8fda61ace88920320c73c979651f9eab8
JonathanJang24 <jonathanjang24@gmail.com>
2021-12-29 08:24:42 -0600

    Initial Commit

---
+++ utils/script.js
@@ -0,0 +1,9 @@
+function navBar() {
+    var x = document.getElementById("navBar")
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+}
