41db302ed5088805162b70795f69d00d60380412
JonathanJang24 <jonathanjang24@gmail.com>
2022-01-04 18:49:37 -0600

    about us done, new pages

--- utils/scrollBehvaior.js
+++ utils/scrollBehvaior.js
@@ -3,10 +3,7 @@ window.addEventListener("scroll", function () {
     let test = document.querySelector("#leadership");
     if (window.scrollY >= 700) {
         console.log("passed");
-        test.style.width = Math.round(window.scrollY / 1) + 'px';
-    }
-    else {
-        test.classList.remove("other")
+        test.style.width = Math.round(window.scrollY / 2) + 'px';
     }
 }
 );
