2c144597e0c07c95e2b6ed6515c3c958c1698ed7
JonathanJang24 <jonathanjang24@gmail.com>
2022-01-23 23:40:32 -0600

    new images, effects, UI/UX improvements

--- utils/scrollBehvaior.js
+++ utils/scrollBehvaior.js
@@ -1,10 +1,33 @@
 window.addEventListener("scroll", function () {
 
-    let test = document.querySelector("#leadership");
-    if (window.scrollY >= 700) {
-        console.log("passed");
-        test.style.width = Math.round(window.scrollY / 2) + 'px';
+    let el = document.querySelector("#leadership");
+
+    let end = window.innerHeight;
+    let mid = end / 2;
+    let elY = el.getBoundingClientRect().y;
+    let tempScale;
+    let tempC;
+
+    if (elY >= 0 && elY <= end) {
+        if (elY >= mid) {
+            tempScale = 30 * (mid / elY);
+            tempC = (end - elY) / mid;
+        }
+
+        else {
+            tempC = (elY) / mid;
+            tempScale = 30 * (elY / mid);
+        }
+        console.log(tempC)
+        el.style = "background:rgb(" + Math.round(100 - (100 * tempC)) + "," + Math.round(160 - (160 * tempC)) + ",190)";
+        el.style.transform = "scale(" + (100 + Math.round(tempScale)) + "%)";
     }
+    else {
+        el.style.transform = "scale(100%)";
+        el.style = "background:rgb(80,120,190)";
+    }
+
+
 }
 );
 
